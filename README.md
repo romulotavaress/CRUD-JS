cb25e83f2b368d4260409d6a56102dbb4f415da6
romulotavaress <romulo-k6@live.com>
2022-07-07 15:39:49 -0300

    Crud em Js

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # CRUD-JS
 Crud simples 
+Adiciona e Remove Usuario cadastrado.
---
+++ nivel-1-crud-javascript-estruturas-de-dados/index.js
@@ -0,0 +1,55 @@
+// [CRUD] JavaScript BÁSICO 
+const miniTwitter = {
+    usuarios: [
+        {
+            username: 'RomuloT',
+        }
+    ],
+    posts: [
+        {
+            id: 1,
+            owner: 'RomuloT',
+            content: 'Meu primeiro tweet'
+        }
+    ],
+};
+// CREATE
+function criaPost(dados) {
+    miniTwitter.posts.push({
+        id: miniTwitter.posts.length + 1,
+        owner: dados.owner,
+        content: dados.content
+    });
+}
+criaPost({ owner: 'RomuloT', content: 'Segundo tweet' }); 
+criaPost({ owner: 'RomuloT', content: 'Terceiro tweet' }); 
+// console.log(miniTwitter.posts)
+
+// READ
+function pegaPosts() {
+    return miniTwitter.posts;
+}
+console.log(pegaPosts())
+
+// UPDATE
+function atualizaContentDoPost(id, novoConteudo) {
+    const postQueVaiSerAtualizado = pegaPosts().find((post) => {
+        return post.id === id;
+    });
+    console.log(postQueVaiSerAtualizado)
+    postQueVaiSerAtualizado.content = novoConteudo
+}
+atualizaContentDoPost(1, 'Novo conteúdo do post')
+console.log(pegaPosts())
+
+// DELETE
+function apagaPost(id) {
+    const listaDePostsAtualizada = pegaPosts().filter((postAtual) => {
+        return postAtual.id !== id;
+    })
+    miniTwitter.posts = listaDePostsAtualizada;
+}
+apagaPost(1);
+apagaPost(2);
+apagaPost(3);
+console.log(pegaPosts());
